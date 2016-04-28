dfee560f0301a1e58c49fec0e73defabae1459b6
Gahee Heo <ghbooth12@gmail.com>
2016-04-28 12:50:41 -0500

    ch5-roadmaps-and-checkpoints: completed

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,14 +1,16 @@
 require 'httparty'
-# require 'json'
+require_relative 'roadmap'
 
 class Kele
+  include HTTParty
+  include Roadmap
   def initialize(email, password)
     @base_uri = 'https://www.bloc.io/api/v1'
     @options = { query: { email: email, password: password } }
   end
 
   def user_token
-    hash = HTTParty.post("#{@base_uri}/sessions", @options)
+    hash = self.class.post("#{@base_uri}/sessions", @options)
     if hash["auth_token"]
       hash["auth_token"]
     else
@@ -17,7 +19,7 @@ class Kele
   end
 
   def get_me
-    response = HTTParty.get("#{@base_uri}/users/me", headers: { "authorization" => user_token })
+    response = self.class.get("#{@base_uri}/users/me", headers: { "authorization" => user_token })
     JSON.parse response.body
   end
 
@@ -26,7 +28,7 @@ class Kele
   end
 
   def get_mentor_availability(id)
-    response = HTTParty.get("#{@base_uri}/mentors/#{id}/student_availability", headers: { "authorization" => user_token })
+    response = self.class.get("#{@base_uri}/mentors/#{id}/student_availability", headers: { "authorization" => user_token })
     JSON.parse response.body
   end
 end
